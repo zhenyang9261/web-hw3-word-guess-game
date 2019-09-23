e7021054f2d28e73016d20df726e77810e52648c
Zhen Data <zhenyang.9261@gmail.com>
2019-09-22 22:21:36 -0400

    Added pseudo code in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,11 +5,11 @@ var words = ["alabama", "alaska", "arizona", "arkansas", "california", "colorado
              "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho",
              "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana",
              "maine", "maryland", "massachusetts", "michigan", "minnesota",
-             "mississippi", "missouri", "montana", "nebraska", "nevada", "new hampshire",
-             "new jersey", "new mexico", "new york", "north carolina", "north dakota", "ohio",
-             "oklahoma", "oregon", "pennsylvania", "rhodeisland", "south carolina",
-             "south dakota", "tennessee", "texas", "utah", "vermont", "virginia",
-             "washington", "west virginia", "wisconsin", "wyoming"];
+             "mississippi", "missouri", "montana", "nebraska", "nevada", "new_hampshire",
+             "new_jersey", "new_mexico", "new_york", "north_carolina", "north_dakota", "ohio",
+             "oklahoma", "oregon", "pennsylvania", "rhodeisland", "south_carolina",
+             "south_dakota", "tennessee", "texas", "utah", "vermont", "virginia",
+             "washington", "west_virginia", "wisconsin", "wyoming"];
 
 var hangman = {
 
@@ -19,9 +19,51 @@ var hangman = {
     remainGuesses: 15,
     win: 0,
 
+    checkLetter: function(letter, word) {
+        for(var i=0; i<word.length;i++) {
+            if (str[i] === letter) 
+                indices.push(i);
+        }
+        return indices;
+    },
+
     play: function(key) {
 
         console.log(key);
+
+        key = key.toLowerCase();
+        var inGuessLetter = this.checkLetter(key, guessedLetters);
+        var inTargetWord = this.checkLetter(key, targetWord);
+
+        // If the letter is not already guessed
+        if (inGuessLetter.length === 0) {
+
+            // If the letter is not in the target word
+            if (inTargetWord.length === 0) {
+
+                // Remaining guess count reduce by 1
+
+                // Remaing guess count is 0
+                
+                    // Display "You lost"
+
+                    // Set gameStarted to false
+            }
+            // If the letter is in the target word
+            else {
+                // Replace currentWord letters at the index positions with the letter
+
+                // Add the letter in the guessedLetters array
+
+                // If the currentWord equals to targetWord
+                    // Game win counter increase by 1
+
+                    // Set gameStarted to false
+            }
+
+        }
+
+        /*
         if (key === 'a') {
             winCount++;
             document.getElementById("result").innerHTML = "Congratuations! You won! <br> Press any key to start a new game";
@@ -30,8 +72,9 @@ var hangman = {
         }
         else {
             document.getElementById("result").innerHTML = "Sorry. You Lost. <br> Press any key to start a new game";
-            document.getElementById("result-img").innerHTML = '';
+            
         }
+        */
     },
 
     reset: function() {
@@ -42,7 +85,6 @@ var hangman = {
 
         var randomNum = Math.floor(Math.random() * 51);
         targetWord = words[randomNum];
-        console.log(targetWord);
 
         currentWord = new Array(targetWord.length + 1).join('_')
 
@@ -50,6 +92,7 @@ var hangman = {
         document.getElementById("current-word").innerHTML = currentWord;
         document.getElementById("guess-remain").innerHTML = this.remainGuesses;
         document.getElementById("guessed-letters").innerHTML = "";
+        document.getElementById("result-img").innerHTML = '';
 
     }
 }
