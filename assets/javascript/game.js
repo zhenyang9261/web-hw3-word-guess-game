b7d87cb8cb019d86cd2280ca88ae646f42004214
Zhen Data <zhenyang.9261@gmail.com>
2019-09-22 23:13:41 -0400

    Added code in place of pseudo code. Not working properly yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,9 +19,11 @@ var hangman = {
     remainGuesses: 15,
     win: 0,
 
+    // Check the position(s) of a letter in a word or array
     checkLetter: function(letter, word) {
+        var indices = [];
         for(var i=0; i<word.length;i++) {
-            if (str[i] === letter) 
+            if (word[i] === letter) 
                 indices.push(i);
         }
         return indices;
@@ -32,49 +34,89 @@ var hangman = {
         console.log(key);
 
         key = key.toLowerCase();
-        var inGuessLetter = this.checkLetter(key, guessedLetters);
-        var inTargetWord = this.checkLetter(key, targetWord);
+        var inGuessLetter = this.checkLetter(key, this.guessedLetters);
+        var inTargetWord = this.checkLetter(key, this.targetWord);
 
         // If the letter is not already guessed
         if (inGuessLetter.length === 0) {
+console.log("not in guessed letters");
+
+
+            // Add the letter in the guessedLetters array
+            this.guessedLetters.push(key);
+
+            // Replace the guessed letters in HTML
+            document.getElementById("guessed-letters").innerHTML = this.guessedLetters.join('');
 
             // If the letter is not in the target word
             if (inTargetWord.length === 0) {
-
+console.log("not in target word");
                 // Remaining guess count reduce by 1
+                this.remainGuesses = this.remainGuesses - 1;
+console.log(this.remainGuesses);
 
                 // Remaing guess count is 0
-                
+                if (this.remainGuess === 0) {
+console.log("Remaining guess 0");
+
                     // Display "You lost"
+                    document.getElementById("result").innerHTML = "Sorry. You Lost. <br> Press any key to start a new game";
 
                     // Set gameStarted to false
+                    gameStarted = false;
+                }
+                // Remain guess count is not 0
+                else {
+console.log("Remaining guess not 0");
+                    
+                    // Replace remain guess counter in HTML
+                    document.getElementById("guess-remain").innerHTML = this.remainGuesses;
+                }
+                
+                 
             }
             // If the letter is in the target word
             else {
-                // Replace currentWord letters at the index positions with the letter
+console.log("in target word  " + inTargetWord);
 
-                // Add the letter in the guessedLetters array
+                // Replace currentWord letters at the index positions with the letter
+                for (var i=0; i<inTargetWord.length; i++) {
+
+                    var pos = inTargetWord[i];
+console.log("pos  " + pos + "  key  " + key);
+
+                    var currentWordArray = this.currentWord.split('');
+                    currentWordArray[pos] = key;
+
+                    this.currentWord = currentWordArray.join('');
+                }
+
+console.log(this.currentWord);
+                // Replace the current word in HTML
+                document.getElementById("current-word").innerHTML = this.currentWord;
 
                 // If the currentWord equals to targetWord
+                if (this.currentWord === this.targetWord) {
+console.log("word guess right");
+                    // Display "You won"
+                    document.getElementById("result").innerHTML = "Congratuations! You won! <br> Press any key to start a new game";
+
+                    // Display state flag
+                    document.getElementById("result-img").innerHTML = '<img src="assets/images/flags-states/' + this.targetWord + '.png" width="100%" height="auto" alt="USA state map">';
+
                     // Game win counter increase by 1
+                    winCount++;
+
+                    // Display new win count in HTML
+                    document.getElementById("win-count").innerHTML = winCount;
 
                     // Set gameStarted to false
+                    this.gameStarted = false;
+                }
             }
 
         }
 
-        /*
-        if (key === 'a') {
-            winCount++;
-            document.getElementById("result").innerHTML = "Congratuations! You won! <br> Press any key to start a new game";
-            document.getElementById("result-img").innerHTML = '<img src="assets/images/flags-states/alabama.png" width="100%" height="auto" alt="USA state map">';
-            document.getElementById("win-count").innerHTML = winCount;
-        }
-        else {
-            document.getElementById("result").innerHTML = "Sorry. You Lost. <br> Press any key to start a new game";
-            
-        }
-        */
     },
 
     reset: function() {
@@ -84,12 +126,12 @@ var hangman = {
         this.remainGuesses = 15;
 
         var randomNum = Math.floor(Math.random() * 51);
-        targetWord = words[randomNum];
-
-        currentWord = new Array(targetWord.length + 1).join('_')
+        this.targetWord = words[randomNum];
+console.log(this.targetWord);
+        this.currentWord = new Array(this.targetWord.length + 1).join('_')
 
         document.getElementById("win-count").innerHTML = winCount;
-        document.getElementById("current-word").innerHTML = currentWord;
+        document.getElementById("current-word").innerHTML = this.currentWord;
         document.getElementById("guess-remain").innerHTML = this.remainGuesses;
         document.getElementById("guessed-letters").innerHTML = "";
         document.getElementById("result-img").innerHTML = '';
