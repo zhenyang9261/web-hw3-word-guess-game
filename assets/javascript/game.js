6ef8388e92115841e659f67f4a573c160782aa9c
Zhen Data <zhenyang.9261@gmail.com>
2019-09-23 20:30:11 -0400

    Corrected some bugs. First working version.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,7 +16,7 @@ var hangman = {
     targetWord: "",
     currentWord: "",
     guessedLetters: [],
-    remainGuesses: 15,
+    remainGuesses: 10,
     win: 0,
 
     // Check the position(s) of a letter in a word or array
@@ -39,8 +39,6 @@ var hangman = {
 
         // If the letter is not already guessed
         if (inGuessLetter.length === 0) {
-console.log("not in guessed letters");
-
 
             // Add the letter in the guessedLetters array
             this.guessedLetters.push(key);
@@ -50,14 +48,12 @@ console.log("not in guessed letters");
 
             // If the letter is not in the target word
             if (inTargetWord.length === 0) {
-console.log("not in target word");
+
                 // Remaining guess count reduce by 1
                 this.remainGuesses = this.remainGuesses - 1;
-console.log(this.remainGuesses);
 
                 // Remaing guess count is 0
-                if (this.remainGuess === 0) {
-console.log("Remaining guess 0");
+                if (this.remainGuesses === 0) {
 
                     // Display "You lost"
                     document.getElementById("result").innerHTML = "Sorry. You Lost. <br> Press any key to start a new game";
@@ -65,25 +61,17 @@ console.log("Remaining guess 0");
                     // Set gameStarted to false
                     gameStarted = false;
                 }
-                // Remain guess count is not 0
-                else {
-console.log("Remaining guess not 0");
-                    
-                    // Replace remain guess counter in HTML
-                    document.getElementById("guess-remain").innerHTML = this.remainGuesses;
-                }
                 
-                 
+                // Replace remain guess counter in HTML
+                document.getElementById("guess-remain").innerHTML = this.remainGuesses;
             }
             // If the letter is in the target word
             else {
-console.log("in target word  " + inTargetWord);
 
                 // Replace currentWord letters at the index positions with the letter
                 for (var i=0; i<inTargetWord.length; i++) {
 
                     var pos = inTargetWord[i];
-console.log("pos  " + pos + "  key  " + key);
 
                     var currentWordArray = this.currentWord.split('');
                     currentWordArray[pos] = key;
@@ -91,13 +79,12 @@ console.log("pos  " + pos + "  key  " + key);
                     this.currentWord = currentWordArray.join('');
                 }
 
-console.log(this.currentWord);
                 // Replace the current word in HTML
                 document.getElementById("current-word").innerHTML = this.currentWord;
 
                 // If the currentWord equals to targetWord
                 if (this.currentWord === this.targetWord) {
-console.log("word guess right");
+
                     // Display "You won"
                     document.getElementById("result").innerHTML = "Congratuations! You won! <br> Press any key to start a new game";
 
@@ -111,23 +98,21 @@ console.log("word guess right");
                     document.getElementById("win-count").innerHTML = winCount;
 
                     // Set gameStarted to false
-                    this.gameStarted = false;
+                    gameStarted = false;
                 }
             }
-
         }
-
     },
 
     reset: function() {
 
         gameStarted = true;
         this.guessedLetters.length = 0;
-        this.remainGuesses = 15;
+        this.remainGuesses = 10;
 
-        var randomNum = Math.floor(Math.random() * 51);
+        var randomNum = Math.floor(Math.random() * (words.length+1));
         this.targetWord = words[randomNum];
-console.log(this.targetWord);
+
         this.currentWord = new Array(this.targetWord.length + 1).join('_')
 
         document.getElementById("win-count").innerHTML = winCount;
@@ -135,6 +120,7 @@ console.log(this.targetWord);
         document.getElementById("guess-remain").innerHTML = this.remainGuesses;
         document.getElementById("guessed-letters").innerHTML = "";
         document.getElementById("result-img").innerHTML = '';
+        document.getElementById("result").innerHTML = '';
 
     }
 }
